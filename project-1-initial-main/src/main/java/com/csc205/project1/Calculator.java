c27c0d5004053dcb687677b77e9ace320548d91d
Trey <tre2147075@maricopa.edu>
2022-02-03 23:45:26 -0700

    Initial commit

---
+++ project-1-initial-main/src/main/java/com/csc205/project1/Calculator.java
@@ -0,0 +1,14 @@
+package com.csc205.project1;
+
+public class Calculator {
+
+    double a;
+    double b;
+
+    class Math(double a, double b, double shiftX, double shiftY) {
+
+    double distance((-2 - b) * (-2 - b) + (4 - a) * (4 - a));
+
+    }
+
+}
---
+++ project-1-initial-main/src/main/java/com/csc205/project1/Point.java
@@ -0,0 +1,33 @@
+package com.csc205.project1;
+
+public class testPoint {
+
+    @testPoint static
+        void shiftX() {
+    }
+
+    @testPoint static
+        void shiftY() {
+    }
+
+    @testPoint static
+        void distance() {
+    }
+
+    @testPoint static
+        void rotate() {
+    }
+
+
+
+    //class point(double a, double b, double shiftX, double y){
+     //   public static void main(String []args) {
+
+       //     return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+
+        //}
+
+
+
+
+}
